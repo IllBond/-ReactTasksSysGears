b2ec444204d10e04ee11946cca6611a4b1c884d5
IlyaBondar <prince.dp.u@gmail.com>
2020-01-28 23:53:00 +0200

    Первая версия первой задачи

---
+++ Task1/task1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Calculator from "./components/Calculator"
+
+
+function App() {
+    return (
+        <div id={'main_container'}>
+            <Calculator />
+        </div>
+    );
+}
+
+export default App;
